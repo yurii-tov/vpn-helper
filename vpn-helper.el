8ee53ea49c8fd98cbe08d0c499bf52d5c63485cd
Yurii <juretz32@yandex.ru>
2021-09-04 17:30:51 +0300

    Initial commit

---
+++ vpn-helper.el
@@ -0,0 +1,57 @@
+(defvar *vpn-settings*)
+(defvar *vpn-server*)
+(defvar *vpn-password*)
+
+
+(define-derived-mode vpn-mode nil "vpn-helper"
+  (setq-local vpn-buffer *vpn-buffer*)
+  (setq-local vpn-server *vpn-server*)
+  (setq-local vpn-password *vpn-password*))
+
+
+(progn
+  (define-key vpn-mode-map (kbd "c") 'vpn-connect)
+  (define-key vpn-mode-map (kbd "d") 'vpn-disconnect)
+  (define-key vpn-mode-map (kbd "s") 'vpn-status))
+
+
+(setq vpn-cli-exe "c:/Program Files (x86)/Cisco/Cisco AnyConnect Secure Mobility Client/vpncli.exe")
+
+
+(defun run-vpn ()
+  (interactive)
+  (let* ((*vpn-server* (read-string "Vpn server: "))
+         (*vpn-password* (read-passwd "Vpn Password: "))
+         (*vpn-buffer* (format "*vpn:%s*" *vpn-server*)))
+    (switch-to-buffer *vpn-buffer*)
+    (vpn-mode)
+    (vpn-status)))
+
+
+(defun vpn-erase-buffer ()
+  (with-current-buffer vpn-buffer
+    (erase-buffer)))
+
+
+(defmacro define-vpn-command (name command)
+  `(defun ,name ()
+     (interactive)
+     (vpn-erase-buffer)
+     (make-process :name ,(concat "vpn-" (symbol-name name))
+                   :buffer vpn-buffer
+                   :command ,command
+                   :coding '(utf-8-dos . utf-8-dos))))
+
+
+(define-vpn-command status (cons vpn-cli-exe '("status")))
+
+
+(define-vpn-command connect
+  '("bash" "-c"
+    (format "echo '%s' | '%s' -s connect %s && mstsc connect.rdp"
+            vpn-password
+            vpn-cli-exe
+            vpn-server)))
+
+
+(define-vpn-command disconnect (cons vpn-cli-exe '("disconnect")))
